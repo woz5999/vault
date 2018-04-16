a0d6adedec32d85c644bbe6332373c749fa11341
Jim Kalafut <jkalafut@hashicorp.com>
2018-04-16 12:14:46 -0700

    Add Responses, attributes, and refactor

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -1048,6 +1048,7 @@ func NewSystemBackend(core *Core, logger log.Logger) *SystemBackend {
 						Type:        framework.TypeInt,
 						Default:     32,
 						Description: "The number of bytes to generate (POST body parameter). Defaults to 32 (256 bits).",
+						Attrs:       map[string]string{"group": "foo"},
 					},
 
 					"format": &framework.FieldSchema{
